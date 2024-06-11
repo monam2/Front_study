6cd28a4586ad09bd0309e777dc169062f5182d14
monam2 <kangcw0107@gmail.com>
2024-06-11 19:19:19 +0900

    0611

--- react/React-Basic/src/components/common/header/CommonHeader.tsx
+++ react/React-Basic/src/components/common/header/CommonHeader.tsx
@@ -4,19 +4,19 @@ import styles from './CommonHeader.module.scss'
 const CommonHeader = () => {
   const navigate = useNavigate();
   
-  const moveToPage = ()=> {
-    navigate('/bookmark')
+  const moveToPage = (filter:string)=> {
+    navigate(filter)
   }
 
   return (
     <header className={styles.header}>
-        <div className={styles.header__logoBox}>
+        <div className={styles.header__logoBox} onClick={()=>moveToPage('/')}>
             <img src="src/assets/images/image-logo.png" alt="" className={styles.header__logoBox__logo} />
             <span className={styles.header__logoBox__title}>PhotoSplash</span>
         </div>
         <div className={styles.header__profileBox}>
             <button className={styles.header__profileBox__button}>사진 제출</button>
-            <button className={styles.header__profileBox__button}  onClick={moveToPage}>북마크</button>
+            <button className={styles.header__profileBox__button}  onClick={()=>moveToPage('/bookmark')}>북마크</button>
             <span className={styles.header__profileBox__userName}>kangcw0107 | @monam2</span>
         </div>
     </header>
--- react/React-Basic/src/pages/bookmark/index.tsx
+++ react/React-Basic/src/pages/bookmark/index.tsx
@@ -1,36 +1,40 @@
-import CommonHeader from "@/components/common/header/CommonHeader"
-import styles from './styles/index.module.scss'
-import { useEffect, useState } from "react"
-import Card from './components/Card';
+import CommonHeader from "@/components/common/header/CommonHeader";
+import styles from "./styles/index.module.scss";
+import { useEffect, useState } from "react";
+import Card from "./components/Card";
 import { CardDTO } from "../index/types/card";
 
 const index = () => {
-    const [data, setData] = useState([]);
-    const getData = ()=>{
-        const getLocalStorage = JSON.parse(localStorage.getItem('bookmark'));
+  const [data, setData] = useState([]);
+  const getData = () => {
+    const getLocalStorage = JSON.parse(localStorage.getItem("bookmark"));
 
-        if (getLocalStorage || getLocalStorage !== null) {
-            setData(getLocalStorage);
-        } else {
-            setData([]);
-        }
+    if (getLocalStorage || getLocalStorage !== null) {
+      setData(getLocalStorage);
+    } else {
+      setData([]);
     }
+  };
 
-    useEffect(()=>{
-        getData();
-    },[])
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <div className={styles.page}>
-        {/* 공통 헤더 UI 부분 */}
-        <CommonHeader />
-        <main className={styles.page__contents}>
-            {data.map((item:CardDTO)=>{
-                return <Card prop={item} key={item.id}/>
-            })}
-        </main>
+      {/* 공통 헤더 UI 부분 */}
+      <CommonHeader />
+      <main className={styles.page__contents}>
+        {data.length === 0 ? (
+          <div className={styles.page__contents__noData}>조회 가능한 데이터가 없습니다.</div>
+        ) : (
+          data.map((item: CardDTO) => {
+            return <Card prop={item} key={item.id} />;
+          })
+        )}
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default index
+export default index;
