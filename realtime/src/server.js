2667f113b8ba7ef542ffd279f445e431f8a63c6c
강창우(monam2) <138136517+monam2@users.noreply.github.com>
2024-06-26 22:41:20 +0900

    Add files via upload

---
+++ realtime/src/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", "src/views");
+app.use("/public", express.static("src/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+const handleListen = () => console.log("Listening on http://localhost:3000");
+app.listen(3000, handleListen);
