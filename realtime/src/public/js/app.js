294429733c58c48f8b4653cdf3c4a753885bb7a5
monam2 <kangcw0107@gmail.com>
2024-06-28 03:59:57 +0900

    0627

--- realtime/src/public/js/app.js
+++ realtime/src/public/js/app.js
@@ -1,1 +1,39 @@
-hi;
+const $ul = document.querySelector("ul");
+const $msgForm = document.querySelector("#message");
+const $nickForm = document.querySelector("#nick");
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+function msgToJson(type, payload) {
+  return JSON.stringify({ type, payload });
+}
+
+socket.addEventListener("open", () => {
+  console.log(">>>>> 서버와 연결됨");
+});
+
+socket.addEventListener("message", ({ data }) => {
+  const li = document.createElement("li");
+  li.innerText = data;
+  $ul.append(li);
+});
+
+socket.addEventListener("close", () => {
+  console.log(">>>>> 서버와 연결 해제됨");
+});
+
+$msgForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = $msgForm.querySelector("input");
+  socket.send(msgToJson("new_message", input.value));
+  input.value = "";
+  input.focus();
+});
+
+$nickForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = $nickForm.querySelector("input");
+  socket.send(msgToJson("nickname", input.value));
+  input.value = "";
+  input.focus();
+});
--- realtime/src/server.js
+++ realtime/src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import { WebSocketServer } from "ws";
 import express from "express";
 
 const app = express();
@@ -8,4 +10,40 @@ app.use("/public", express.static("src/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+const sockets = [];
+
+function handleConnection(socket) {
+  sockets.push(socket);
+  socket["nickname"] = "annonymous";
+  console.log(">>>>> 브라우저와 연결됨");
+  socket.send("hello");
+
+  socket.on("close", () => {
+    console.log(">>>>> 브라우저와 연결 해제됨");
+  });
+
+  socket.on("message", (message) => {
+    const { type, payload } = JSON.parse(message);
+    switch (type) {
+      case "new_message":
+        sockets.forEach((sckt) => sckt.send(`${socket.nickname} : ${payload}`));
+        break;
+      case "nickname":
+        socket["nickname"] = payload;
+        break;
+    }
+
+    // if (parsed.type === "new_message") {
+    //   sockets.forEach((sckt) => sckt.send(parsed.payload));
+    // } else if (parsed.type === "nickname") {
+    //   console.log(parsed.payload);
+    // }
+  });
+}
+
+wss.on("connection", handleConnection);
+server.listen(3000, handleListen);
